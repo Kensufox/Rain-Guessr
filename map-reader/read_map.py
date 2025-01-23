ce8a60287e1b56dad0d5415675d83394cb2385f4
Kensufox <pokenfox@gmail.com>
2025-01-23 20:30:02 +0100

    make the python script work

--- map-reader/read_map.py
+++ map-reader/read_map.py
@@ -34,25 +34,27 @@ def render_ascii_art(width, height, geometry_data):
     cell = 0
     strange_value = []
 
-    for x in range(width):
-        for y in range(height):
+    for y in range(height):
+        for x in range(width):
+            x_val = width - x - 1
+            y_val = height - y - 1
             try :
-                cell = float(geometry_data[x+(y*width)].replace(",", "."))
+                cell = float(geometry_data[y+(x*height)].replace(",", "."))
             except ValueError:
                 cell = -1
-                strange_value.append(geometry_data[x+(y*width)])
+                strange_value.append(geometry_data[y+(x*height)])
             if cell == 0:
-                grid[y][x] = '□'  # Air
+                grid[y_val][x_val] = '□'  # Air
             elif cell == 1:
-                grid[y][x] = '#'  # Mur
+                grid[y_val][x_val] = '#'  # Mur
             elif cell == 2:
-                grid[y][x] = '~'  # Eau
+                grid[y_val][x_val] = '~'  # Eau
             elif cell == 3:
-                grid[y][x] = '|'  # Poteaux verticaux
+                grid[y_val][x_val] = '|'  # Poteaux verticaux
             elif cell == 4:
-                grid[y][x] = '-'  # Poteaux horizontaux
+                grid[y_val][x_val] = '-'  # Poteaux horizontaux
             else:
-                grid[y][x] = '?'  # Inconnu
+                grid[y_val][x_val] = '?'  # Inconnu
 
     print(strange_value)
     # Génération du rendu ASCII
